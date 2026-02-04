e8ecd3cb34626cdd29e4730081f94dbffb9f65e1
Jad Dina <jad.dina19@gmail.com>
2026-02-03 22:38:52 -0700

    added task instances to main

--- Components/main_system.cpp
+++ Components/main_system.cpp
@@ -12,6 +12,12 @@
 // Tasks
 #include "CubeTask.hpp"
 #include "DebugTask.hpp"
+#include "IMUTask.hpp"
+#include "LSM6DSOTask.hpp"
+#include "mmc5983Task.hpp"
+#include "BaroTask07.hpp"
+#include "BaroTask11.hpp"
+#include "LoggingTask.hpp"
 
 /* Drivers ------------------------------------------------------------------*/
 namespace Driver {
@@ -26,6 +32,13 @@ void run_main() {
     // Init Tasks
     CubeTask::Inst().InitTask();
     DebugTask::Inst().InitTask();
+    IMUTask::Inst().InitTask();
+    LSM6DSOTask::Inst().InitTask();
+    MMC5983MATask::Inst().InitTask();
+    BaroTask07::Inst().InitTask();
+    BaroTask11::Inst().InitTask();
+    LoggingTask::Inst().InitTask();
+
 
     // Print System Boot Info : Warning, don't queue more than 10 prints before scheduler starts
     SOAR_PRINT("\n-- CUBE SYSTEM --\n");
